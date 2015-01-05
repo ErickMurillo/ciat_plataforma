69e1b889812b27bdbb87ccf26c09ddc5db60a20c
Carlos Rocha <crocha09.09@gmail.com>
2015-01-05 10:26:13 -0600

    agregando el local settings

--- ciat_plaforma/settings.py
+++ ciat_plaforma/settings.py
@@ -8,9 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from local_settings import *
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,16 +49,6 @@ ROOT_URLCONF = 'ciat_plaforma.urls'
 
 WSGI_APPLICATION = 'ciat_plaforma.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
