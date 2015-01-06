1314eb4f807a4ebda4d5c892d740ef78ef88f995
Carlos Rocha <crocha09.09@gmail.com>
2015-01-06 11:30:13 -0600

    agregando pagina principal

--- ciat_plaforma/urls.py
+++ ciat_plaforma/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ciat_plaforma.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name="indexgeneral.html")),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
