db81852f3ed262ff54d36f8424eb8fa97a6bddcd
Carlos Rocha <crocha09.09@gmail.com>
2015-01-09 16:28:14 -0600

    fix name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciat_plaforma.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciat_plataforma.settings")
 
     from django.core.management import execute_from_command_line
 
