de84918c0a15be657c27cde96f98e69ff4ada262
Carlos Rocha <crocha09.09@gmail.com>
2015-04-06 17:05:15 -0600

    agregando filtro de analisis

--- analisis/analisis/forms.py
+++ analisis/analisis/forms.py
@@ -4,6 +4,8 @@ from models import *
 from django import forms
 from django.forms import ModelForm
 from ajax_select import make_ajax_field
+from comunicacion.lugar.models import Pais
+from analisis.configuracion.models import SitioAccion, Tipo_Estudio
 
 class Pregunta_5aForm(forms.ModelForm):
     #prioritizado = forms.IntegerField(widget=forms.Select(attrs={'class':'select-evt'}))
@@ -35,3 +37,31 @@ class Pregunta_6aForm(forms.ModelForm):
 #     def __init__(self, *args, **kwargs):
 #         super(Pregunta_5eForm, self).__init__(*args, **kwargs)
 #         self.fields['innovacion'].queryset = Pregunta_5a.objects.filter(prioritizado='1')
+
+FECHA_CHOICES = (
+					(1,2014),
+					(2,2015),
+					(3,2016),
+					(4,2017),
+					(5,2018),
+					(6,2019),
+					(7,2020),
+					(8,2021),
+					(9,2022),
+					(10,2023),
+					(11,2024),
+				)
+
+class EntrevistaConsulta(forms.Form):
+	fecha = forms.ChoiceField(choices=FECHA_CHOICES)
+	pais = forms.ModelChoiceField(queryset=Pais.objects.order_by('-nombre'), 
+								  required=False, 
+			                      label=u'Pais')
+	sitio_accion = forms.ModelChoiceField(queryset=SitioAccion.objects.order_by('-nombre'), 
+								  required=False, 
+			                      label=u'Sitio de acción')
+	tipo_estudio = forms.ModelChoiceField(queryset=Tipo_Estudio.objects.order_by('-nombre'), 
+								  required=False, 
+			                      label=u'Tipo de estudio')
+
+
--- analisis/analisis/views.py
+++ analisis/analisis/views.py
@@ -5,64 +5,119 @@ from .models import *
 from mapeo.models import *
 from django.core import serializers
 from django.http import HttpResponse
+from .forms import *
 
-# Create your views here.
+#Inicio del filtro para la consultas del front-end
+
+def _queryset_filtrado(request):
+    params = {}
+    if 'fecha' in request.session:
+        params['fecha1'] = request.session['fecha1']
+
+    if 'pais' in request.session:
+        params['pais'] = request.session['pais']
+
+    if 'sitio_accion' in request.session:
+        params['organizacion__sitio_accion'] = request.session['sitio_accion']
+
+    if 'tipo_estudio' in request.session:
+        params['tipo_estudio'] = request.session['tipo_estudio']
+
+    unvalid_keys = []
+    for key in params:
+        if not params[key]:
+            unvalid_keys.append(key)
+
+    for key in unvalid_keys:
+        del params[key]
+
+    return Entrevista.objects.filter(**params)
+
+def inicio(request, template='analisis/inicio.html'):
+
+    if request.method == 'POST':
+        mensaje = None
+        form = EntrevistaConsulta(request.POST)
+        if form.is_valid():
+            request.session['fecha'] = form.cleaned_data['fecha']
+            request.session['pais'] = form.cleaned_data['pais']
+            request.session['sitio_accion'] = form.cleaned_data['sitio_accion']
+            request.session['tipo_estudio'] = form.cleaned_data['tipo_estudio']
+
+            mensaje = "Todas las variables estan correctamente :)"
+            request.session['activo'] = True
+            centinela = 1
+        else:
+            centinela = 0   
+           
+    else:
+        form = EntrevistaConsulta()
+        mensaje = "Existen alguno errores"
+        centinela = 0
+        if 'fecha' in request.session:
+            del request.session['fecha']
+            del request.session['pais']
+            del request.session['sitio_accion']
+            del request.session['tipo_estudio']
+    
+    return render(request, template, locals())
+
 class IndexView(ListView):
-	template_name = 'analisis/index.html'
-	model = Sector
+    template_name = 'analisis/index.html'
+    model = Sector
 
-	def get_context_data(self,**kwargs):
-		context = super(IndexView, self).get_context_data(**kwargs)
-		contador_org = Organizaciones.objects.count()
+    def get_context_data(self,**kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        contador_org = Organizaciones.objects.count()
 
-		sector = {}
-		for x in Sector.objects.all():
-			cont_org = Organizaciones.objects.filter(sector=x).count()
-			sector[x.nombre] = cont_org
-		context['contador_sector'] = sector
+        sector = {}
+        for x in Sector.objects.all():
+            cont_org = Organizaciones.objects.filter(sector=x).count()
+            sector[x.nombre] = cont_org
+        context['contador_sector'] = sector
 
-		tema = {}
-		for y in Tema.objects.all():
-			contador_pregunta1 = Pregunta_1.objects.filter(tema=y).count()
-			tema[y.tema] = contador_pregunta1
-		context['contador_tematica'] = tema
-		
-		prueba = {}
-		for i in Sector.objects.all():
-			conteo = Pregunta_1.objects.filter(entrevistado__organizacion__sector=i)
-			prueba[i.nombre] = conteo
-		context['proyectos'] = prueba
-			
-		impactos = {}
-		for imp in Sector.objects.all():
-			preg_4 = Pregunta_4.objects.filter(entrevistado__organizacion__sector=imp).count()
-			impactos[imp.nombre] = preg_4
-		context['impacto'] = impactos
+        tema = {}
+        for y in Tema.objects.all():
+            contador_pregunta1 = Pregunta_1.objects.filter(tema=y).count()
+            tema[y.tema] = contador_pregunta1
+        context['contador_tematica'] = tema
+        
+        prueba = {}
+        for i in Sector.objects.all():
+            conteo = Pregunta_1.objects.filter(entrevistado__organizacion__sector=i)
+            prueba[i.nombre] = conteo
+        context['proyectos'] = prueba
+            
+        impactos = {}
+        for imp in Sector.objects.all():
+            preg_4 = Pregunta_4.objects.filter(entrevistado__organizacion__sector=imp).count()
+            impactos[imp.nombre] = preg_4
+        context['impacto'] = impactos
 
-		preg_4_tema = {}
-		for y in Tema.objects.all():
-			contador_pregunta4 = Pregunta_4.objects.filter(tema=y).count()
-			preg_4_tema[y.tema] = contador_pregunta4
-		context['impactos_tematica'] = preg_4_tema
+        preg_4_tema = {}
+        for y in Tema.objects.all():
+            contador_pregunta4 = Pregunta_4.objects.filter(tema=y).count()
+            preg_4_tema[y.tema] = contador_pregunta4
+        context['impactos_tematica'] = preg_4_tema
 
-		innocacion = {}
-		for inno in Sector.objects.all():
-			contador_pregunta5 = Pregunta_5a.objects.filter(entrevistado__organizacion__sector=inno).count()
-			innocacion[inno.nombre] = contador_pregunta5
-		context['innovacion_contador']  = innocacion
+        innocacion = {}
+        for inno in Sector.objects.all():
+            contador_pregunta5 = Pregunta_5a.objects.filter(entrevistado__organizacion__sector=inno).count()
+            innocacion[inno.nombre] = contador_pregunta5
+        context['innovacion_contador']  = innocacion
 
-		tematica_innovacion = {}
-		for ti in Tema.objects.all():
-			contador_tematica_5a = Pregunta_5a.objects.filter(tema=ti).count()
-			tematica_innovacion[ti.tema] = contador_tematica_5a
-		context['tematica_innovacion_5a'] = tematica_innovacion
+        tematica_innovacion = {}
+        for ti in Tema.objects.all():
+            contador_tematica_5a = Pregunta_5a.objects.filter(tema=ti).count()
+            tematica_innovacion[ti.tema] = contador_tematica_5a
+        context['tematica_innovacion_5a'] = tematica_innovacion
 
-		return context
+        return context
 
 class BusquedaPaisView(TemplateView):
-	 
-	def get(self, request, *args, **kwargs):
-		id_pais = request.GET['id']
-		departamento = Departamento.objects.filter(pais__id=id_pais)
-		data = serializers.serialize('json',departamento,fields=('nombre',))
-		return HttpResponse(data,mimetype='application/json')
+     
+    def get(self, request, *args, **kwargs):
+        id_pais = request.GET['id']
+        departamento = Departamento.objects.filter(pais__id=id_pais)
+        data = serializers.serialize('json',departamento,fields=('nombre',))
+        return HttpResponse(data,mimetype='application/json')
