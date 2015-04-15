b88c8b9307391c4569b47de16665920ddf324f42
Carlos Rocha <crocha09.09@gmail.com>
2015-04-15 11:08:44 -0600

    fix urls post no debe existir

--- analisis/analisis/urls.py
+++ analisis/analisis/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 
 urlpatterns = patterns('analisis.analisis.views',
 	url(r'^$', 'inicio', name="inicio"),
-    url(r'^salidas/$', 'post', name='salidas'),
+    #url(r'^salidas/$', 'post', name='salidas'),
     url(r'^salida1/$', 'salida1', name='salida1'),
     url(r'^salida2/$', 'salida2', name='salida2'),
     url(r'^salida3/$', 'salida3', name='salida3'),
