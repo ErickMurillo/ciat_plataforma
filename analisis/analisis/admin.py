7a880c716fe77f484e77f1e97f99de44821ec64e
Carlos Rocha <crocha09.09@gmail.com>
2015-04-29 13:49:18 -0600

    fix

--- analisis/analisis/admin.py
+++ analisis/analisis/admin.py
@@ -331,7 +331,8 @@ class EntrevistaAdmin(NestedModelAdmin):
 	fieldsets = [
 		(_('Informacion de la persona entrevistada'), {'fields' : (('nombre','posicion','email','organizacion','pais','departamento','telefono'),('fecha1','alcance1','tipo_estudio',))}),
 	]
-	list_display = ('nombre','posicion','organizacion','fecha1','tipo_estudio')
+	list_display = ('organizacion','nombre','posicion','fecha1','tipo_estudio')
+	list_display_links = ('organizacion','nombre')
 	list_filter = ('pais',)
 	inlines = [Pregunta_1_Inline, Pregunta_2_Inline, Pregunta_3_Inline, Pregunta_4_Inline, 
 			   Pregunta_5a_Inline, Pregunta_5c_Inline, Pregunta_5d_Inline, Pregunta_5e_Inline,
