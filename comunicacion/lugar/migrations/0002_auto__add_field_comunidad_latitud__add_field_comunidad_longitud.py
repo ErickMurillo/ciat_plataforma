45dd3a027b2da70b00dfab237fd6932ccfa22e30
Carlos Rocha <crocha09.09@gmail.com>
2015-09-02 23:33:19 -0600

    nuevo falguni de mapeo inconcluso

---
+++ comunicacion/lugar/migrations/0002_auto__add_field_comunidad_latitud__add_field_comunidad_longitud.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Comunidad.latitud'
+        db.add_column(u'lugar_comunidad', 'latitud',
+                      self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=8, decimal_places=5, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Comunidad.longitud'
+        db.add_column(u'lugar_comunidad', 'longitud',
+                      self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=8, decimal_places=5, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Comunidad.latitud'
+        db.delete_column(u'lugar_comunidad', 'latitud')
+
+        # Deleting field 'Comunidad.longitud'
+        db.delete_column(u'lugar_comunidad', 'longitud')
+
+
+    models = {
+        u'lugar.comunidad': {
+            'Meta': {'ordering': "['nombre']", 'object_name': 'Comunidad'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'longitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'municipio': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['lugar.Municipio']"}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '40'})
+        },
+        u'lugar.departamento': {
+            'Meta': {'ordering': "['nombre']", 'object_name': 'Departamento'},
+            'extension': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'pais': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['lugar.Pais']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True'})
+        },
+        u'lugar.municipio': {
+            'Meta': {'ordering': "['departamento__nombre', 'nombre']", 'object_name': 'Municipio'},
+            'departamento': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['lugar.Departamento']"}),
+            'extension': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
+            'latitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'longitud': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'unique': 'True', 'null': 'True'})
+        },
+        u'lugar.pais': {
+            'Meta': {'object_name': 'Pais'},
+            'codigo': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nombre': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['lugar']
