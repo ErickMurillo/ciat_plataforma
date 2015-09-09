2f0a56ffd1ba5196df3eb03cb91d039d0a0dec48
Carlos Rocha <crocha09.09@gmail.com>
2015-09-09 16:18:30 -0600

    agregando mapa con los actores

--- mapeo/views.py
+++ mapeo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .models import *
 from .forms import MapeoConsulta, MapaConsulta
 import json
+from itertools import chain
 
 # Create your views here.
 
@@ -65,36 +66,70 @@ def proyectos(request, template="mapeo/proyectos.html"):
 def mapa_actores(request, template="mapeo/mapa.html", id_proyecto=None):
 
     el_proyecto = Proyectos.objects.get(id=id_proyecto)
-    form_mapa = MapaConsulta()
+    if request.method == 'POST':
+        mensaje = None
+        form_mapa = MapaConsulta(request.POST)
+        if form_mapa.is_valid():
+            request.session['tipo_persona'] = form_mapa.cleaned_data['tipo_persona']
+            request.session['activo'] = True
+            centinela = 1
+        else:
+            centinela = 0
+
+    else:
+        form_mapa = MapaConsulta()
+        mensaje = "Existen alguno errores"
+        centinela = 0
+        try:
+            del request.session['tipo_persona']
+        except:
+            pass
 
     return render(request, template, locals())
 
-
+ 
 def obtener_mapa(request, id_proyecto=None):
-    todos_productores = Productor.objects.filter(proyecto__id=id_proyecto)
-    todos_lideres = Lideres.objects.filter(proyecto__id=id_proyecto)
-    todos_tecnicoespinvestigador = TecnicoEspInvestigador.objects.filter(proyecto__id=id_proyecto)
-    todos_decisor = Decisor.objects.filter(proyecto__id=id_proyecto)
-
-    #todos_juntos = pass
-    print "aca esta el comentarios"
-    print request.POST
+    persona_productor = Persona.objects.filter(productor__proyecto__id=id_proyecto)
+    persona_lideres = Persona.objects.filter(lideres__proyecto__id=id_proyecto)
+    persona_tecnicoespinvestigador = Persona.objects.filter(tecnicoespinvestigador__proyecto__id=id_proyecto)
+    persona_decisor = Persona.objects.filter(decisor__proyecto__id=id_proyecto)
+    
+    todos = list(chain(persona_productor, persona_lideres, persona_tecnicoespinvestigador,persona_decisor))
+    
     if request.is_ajax():
         lista = []
-        print request.POST
-
         params = []
-        # if request.POST['tipo_persona'] == 0:
-        #     params = consulta.filter(fkmercado__departamento__id=request.POST['depart'])
-        # else:
-        #     params = ActividadMercado.objects.filter(fkmercado__departamento__id=request.POST['depart'])
-        for objeto in Persona.objects.all():
-            dicc = dict(nombre=objeto.nombre,
-                        id=objeto.id,
-                        lon=float(objeto.municipio.longitud) ,
-                        lat=float(objeto.municipio.latitud),
-                        )
-            lista.append(dicc)
+        try:
+            if request.POST['valor_persona'] == '1':
+                params = Persona.objects.filter(productor__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+            if request.POST['valor_persona'] == '2':
+                params = Persona.objects.filter(lideres__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+            if request.POST['valor_persona'] == '3':
+                params = Persona.objects.filter(tecnicoespinvestigador__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+            if request.POST['valor_persona'] == '4':
+                params = Persona.objects.filter(tecnicoespinvestigador__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+            if request.POST['valor_persona'] == '5':
+                params = Persona.objects.filter(tecnicoespinvestigador__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+            if request.POST['valor_persona'] == '6':
+                params = Persona.objects.filter(decisor__proyecto__id=id_proyecto, tipo_persona=request.POST['valor_persona'])
+             
+            for objeto in params:
+                dicc = dict(nombre=objeto.nombre,
+                            tipo=objeto.tipo_persona,
+                            id=objeto.id,
+                            lon=float(objeto.municipio.longitud) ,
+                            lat=float(objeto.municipio.latitud),
+                            )
+                lista.append(dicc)
+        except:
+            for objeto in todos:
+                dicc = dict(nombre=objeto.nombre,
+                            tipo=objeto.tipo_persona,
+                            id=objeto.id,
+                            lon=float(objeto.municipio.longitud) ,
+                            lat=float(objeto.municipio.latitud),
+                            )
+                lista.append(dicc)
 
         serializado = json.dumps(lista)
         return HttpResponse(serializado, content_type='application/json')
