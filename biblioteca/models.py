776a8aefd603242c618eaa417f541e61445308f1
Carlos Rocha <crocha09.09@gmail.com>
2015-09-16 23:09:46 -0600

    agregando campo al modelo de bibblioteca

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from comunicacion.utils import *
 from mapeo.models import Proyectos
 from analisis.configuracion.models import Plataforma
+from mapeo.models import Temas
 
 # Create your models here.
 CHOICE_TIPO = (
@@ -40,6 +41,7 @@ class Biblioteca(models.Model):
     palabras_claves = models.CharField(max_length=250, null=True, blank=True)
     proyecto = models.ManyToManyField(Proyectos, blank=True)
     alianza = models.ManyToManyField(Plataforma, blank=True)
+    temas = models.ManyToManyField(Temas, blank=True)
 
     user = models.ForeignKey(User)
 
