fbe68a110987f3c4bc875602eab23b1bcbc5dd41
Carlos Rocha <crocha09.09@gmail.com>
2015-09-25 16:15:45 -0600

    viendo cmo hacer las busqueda por temas en biblioteca

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Biblioteca
 from django.db.models import Q
-
+from forms import *
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -11,13 +11,15 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['resultados'] = Biblioteca.objects.all()
+        context['form'] = BibliotecaConsulta()
 
         return context
 
 def buscador(request):
+    form = BibliotecaConsulta()
     if request.method == "GET":
         search_text = request.GET['q']
-        print search_text
+        search_text2 = request.GET['temas']
         if search_text is not None and search_text != u"":
             search_text = request.GET['q']
             resultados = Biblioteca.objects.filter(Q(titulo__icontains=search_text) |
@@ -31,7 +33,7 @@ def buscador(request):
         else:
             resultados = []
 
-    return render(request, 'biblioteca/index.html', {'resultados':resultados})
+    return render(request, 'biblioteca/index.html', {'resultados':resultados, 'form':form})
 
 
 def detalle_libro(request, id_libro=None, template="biblioteca/detail-biblioteca.html"):
