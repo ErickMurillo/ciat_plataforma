420d11fc29126604be00407851b2797ac9f03103
Erick Murillo <erickmurillo22@gmail.com>
2016-09-28 15:55:51 -0600

    reparaciones salidas gb 70%

--- ficha_granos_basicos/views.py
+++ ficha_granos_basicos/views.py
@@ -127,8 +127,6 @@ def composicion_familiar(request,template="granos_basicos/productores/composicio
 	filtro = _queryset_filtrado(request)
 	productores = filtro.distinct('productor').count()
 
-	count_productores = filtro.distinct('productor').count()
-
 	#nuevas salidas
 	lista_hijos = []
 	lista_hijas = []
@@ -169,25 +167,7 @@ def composicion_familiar(request,template="granos_basicos/productores/composicio
 		                    productor__composicionfamiliar__escolaridad = obj[0]).distinct('productor__composicionfamiliar').count()
 		padre = filtro.filter(productor__composicionfamiliar__familia = '1',
 		                    productor__composicionfamiliar__escolaridad = obj[0]).distinct('productor__composicionfamiliar').count()
-		escolaridad[obj[1]] = (madre,padre)
-	#--------------------------------------------------------------------------------
 
-
-
-
-	escolaridad_hijos = {}
-	for obj in ESCOLARIDAD_CHOICES:
-		padre = filtro.filter(productor__composicionfamiliar__familia = '1',
-		                    productor__composicionfamiliar__escolaridad = obj[0]).distinct('productor__composicionfamiliar').count()
-		percentage_padre = (padre / count_productores) * 100
-
-		madre = filtro.filter(productor__composicionfamiliar__familia = '2',
-		                    productor__composicionfamiliar__escolaridad = obj[0]).distinct('productor__composicionfamiliar').count()
-		percentage_madre = (madre / count_productores) * 100
-
-		# escolaridad[obj[1]] = (percentage_padre,percentage_madre)
-
-		#--------------------------------
 		#hijos--------------------
 		hijos_5_12 = filtro.filter(productor__composicionfamiliar__familia = '3',
 									productor__composicionfamiliar__escolaridad = obj[0],
@@ -214,28 +194,32 @@ def composicion_familiar(request,template="granos_basicos/productores/composicio
 									productor__composicionfamiliar__escolaridad = obj[0],
 									productor__composicionfamiliar__edad__range = (19,100)).distinct('productor__composicionfamiliar').count()
 
-		escolaridad_hijos[obj[1]] = (saca_porcentajes(hijas_5_12,hijas,False),
-									saca_porcentajes(hijas_13_18,hijas,False),
-									saca_porcentajes(hijas_19,hijas,False),
 
-									saca_porcentajes(hijos_5_12,hijos,False),
-									saca_porcentajes(hijos_13_18,hijos,False),
-									saca_porcentajes(hijos_19,hijos,False))
+		escolaridad[obj[1]] = (madre,padre,
+								hijos_5_12,hijos_13_18,hijos_19,
+								hijas_5_12,hijas_13_18,hijas_19)
+	#--------------------------------------------------------------------------------
 
 	SI_NO_CHOICES = ((1,'Si'),(2,'No'))
+	FAMILIA_CHOICES = ((1,'Padre'),(2,'Madre'),(3,'Hijo'),(4,'Hija'),(5,'Hermano'),
+	    				(6,'Hermana'),(7,'Sobrino'),(8,'Sobrina'),(9,'Abuelo'),
+	    				(10,'Abuela'),(11,'Cuñado'),(12,'Cuñada'),(13,'Yerno'),
+						(14,'Nuera'),(15,'Otro'),)
 
-	participacion = {}
-	for obj in SI_NO_CHOICES:
-		participan_hijos = filtro.filter(productor__composicionfamiliar__familia = '3',
-									productor__composicionfamiliar__participacion = obj[0]).distinct(
+	list_participacion = []
+
+	for obj in FAMILIA_CHOICES:
+		total = filtro.filter(productor__composicionfamiliar__familia = obj[0]).distinct(
 									'productor__composicionfamiliar').count()
 
-		participan_hijas = filtro.filter(productor__composicionfamiliar__familia = '4',
-									productor__composicionfamiliar__participacion = obj[0]).distinct(
+		si_participa = filtro.filter(productor__composicionfamiliar__familia = obj[0],
+									productor__composicionfamiliar__participacion = '1').distinct(
 									'productor__composicionfamiliar').count()
 
-		participacion[obj[1]] = (saca_porcentajes(participan_hijos,hijos,False),
-								saca_porcentajes(participan_hijas,hijas,False))
+		promedio = total / float(productores)
+		promedio = round(promedio, 2)
+
+		list_participacion.append((obj[1],saca_porcentajes(si_participa,total,False),promedio))
 
 	return render(request, template, locals())
 
@@ -251,13 +235,34 @@ def caracteristicas_parcela(request,template="granos_basicos/monitoreos/caracter
 	filtro = _queryset_filtrado(request)
 	productores = filtro.distinct('productor').count()
 
-	parcela_5 = filtro.filter(edad_parcela__range = (0,5)).aggregate(avg = Avg('profundidad_capa'))['avg']
-	parcela_6_20 = filtro.filter(edad_parcela__range = (6,20)).aggregate(avg = Avg('profundidad_capa'))['avg']
-	parcela_20 = filtro.filter(edad_parcela__range = (21,100)).aggregate(avg = Avg('profundidad_capa'))['avg']
+	lista_parcela = []
+	lista_inclinado = []
+	lista_plano = []
 
-	#grafico de lineas
-	inclinado = filtro.filter(distribucionpendiente__seleccion = '1',distribucionpendiente__inclinado__gt = 59.9).values_list('profundidad_capa','distribucionpendiente__inclinado')
-	plano = filtro.filter(distribucionpendiente__seleccion = '1',distribucionpendiente__plano__gt = 59.9).values_list('profundidad_capa','distribucionpendiente__plano')
+	#edad parcela y profundidad capa arable
+	parcela = filtro.values_list('edad_parcela','profundidad_capa')
+	for obj in parcela:
+		lista_parcela.append(parcela)
+
+	#edad de las parcelas
+	menor_5 = filtro.filter(edad_parcela__range = (0,5)).count()
+	edad_6_20 = filtro.filter(edad_parcela__range = (5.1,20)).count()
+	mayor_20 = filtro.filter(edad_parcela__range = (20.1,100)).count()
+
+	for obj in filtro:
+		# % area inclinado > 60%
+		area = DistribucionPendiente.objects.filter(monitoreo = obj,seleccion = '1').values_list('inclinado',flat = True)
+		for x in area:
+			if x >= 60:
+				inclinado = DistribucionPendiente.objects.filter(monitoreo = obj,seleccion = '2').values_list('inclinado','monitoreo__profundidad_capa')
+				lista_inclinado.append(inclinado)
+
+		# % area plano > 60%
+		area1 = DistribucionPendiente.objects.filter(monitoreo = obj,seleccion = '1').values_list('plano',flat = True)
+		for y in area1:
+			if y >= 60:
+				plano = DistribucionPendiente.objects.filter(monitoreo = obj,seleccion = '2').values_list('plano','monitoreo__profundidad_capa')
+				lista_plano.append(plano)
 
 	#acceso agua
 	SI_NO_CHOICES = ((1,'Si'),(2,'No'))
@@ -281,34 +286,82 @@ def ciclo_productivo(request,template="granos_basicos/monitoreos/ciclo_productiv
 	filtro = _queryset_filtrado(request)
 	productores = filtro.distinct('productor').count()
 
+	#siembra
+	fecha_siembra = filtro.values_list('datosmonitoreo__fecha_siembra',flat = True)
+	lista_siembra = []
+	for obj in fecha_siembra:
+		if obj != None:
+			x = obj.isocalendar()[1]
+			lista_siembra.append(x)
+	l_siembra = sorted(lista_siembra)
+
+	dic_siembra = collections.OrderedDict()
+	for v in l_siembra:
+		count = l_siembra.count(v)
+		dic_siembra[v] = count
+
+	#cosecha
+	fecha_cosecha = filtro.values_list('datosmonitoreo__fecha_cosecha',flat = True)
+	lista_cosecha = []
+	for obj in fecha_cosecha:
+		if obj != None:
+			x = obj.isocalendar()[1]
+			lista_cosecha.append(x)
+	l_cosecha = sorted(lista_cosecha)
+
+	dic_cosecha = collections.OrderedDict()
+	for v in l_cosecha:
+		count = l_cosecha.count(v)
+		dic_cosecha[v] = count
+
 	return render(request, template, locals())
 
 def uso_suelo(request,template="granos_basicos/monitoreos/uso_suelo.html"):
 	filtro = _queryset_filtrado(request)
 	productores = filtro.distinct('productor').count()
 
-	avg_fincas = filtro.filter(productor__usosuelo__uso = '1').distinct().aggregate(avg = Avg('productor__usosuelo__cantidad'))['avg']
-
-	avg_cultivos_anuales = filtro.filter(productor__usosuelo__uso = '2').distinct().aggregate(avg = Avg('productor__usosuelo__cantidad'))['avg']
-
-	USO_SUELO_CHOICES = ((2,'Cultivos Anuales (GB)'),(3,'Cultivos perennes'),
+	USO_SUELO_CHOICES = ((1,'Área Total'),(2,'Cultivos Anuales (GB)'),(3,'Cultivos perennes'),
 	    (4,'Tacotales'),(5,'Potreros'),(6,'Pasto de Corte'))
 
-	areas_finca = collections.OrderedDict()
-	areas = filtro.filter(productor__usosuelo__uso = '1').distinct('productor__productor').values_list('productor__usosuelo__cantidad', flat = True)
-	total_areas = 0
-	for x in areas:
-		total_areas += x
-
+	total = filtro.filter(productor__usosuelo__uso = '1').aggregate(total = Sum('productor__usosuelo__cantidad'))['total']
+	uso_suelo = collections.OrderedDict()
 	for obj in USO_SUELO_CHOICES:
-		area = 0
-		values = filtro.filter(productor__usosuelo__uso = obj[0]).values_list('productor__usosuelo__cantidad', flat = True)
-		if values == None:
-			values = 0
-		for y in values:
-			area += y
+		#tabla 1
+		familias = filtro.filter(productor__usosuelo__uso = obj[0]).count()
+		mz = filtro.filter(productor__usosuelo__uso = obj[0]).aggregate(total = Sum('productor__usosuelo__cantidad'))['total']
+		porcentaje = saca_porcentajes(mz,total,False)
+		try:
+			promedio = mz / float(familias)
+		except:
+			promedio = 0
 
-		areas_finca[obj[1]] = (area,saca_porcentajes(area,total_areas,False))
+		uso_suelo[obj[1]] = (familias,mz,porcentaje,promedio)
+
+	#tabla 2
+	tamano_finca = filtro.filter(productor__usosuelo__uso = '1').values_list('productor__usosuelo__cantidad',flat = True)
+	granos_basicos = filtro.filter(productor__usosuelo__uso = '2').values_list('productor__usosuelo__cantidad',flat = True)
+	area_siembra = filtro.values_list('datosmonitoreo__area_siembra',flat = True)
+
+	result = []
+	#promedio,mediana,desviacion standard, minimo y maximo
+	promedios = [np.mean(tamano_finca),np.mean(granos_basicos),np.mean(area_siembra)]
+	mediana = [np.median(tamano_finca),np.median(granos_basicos),np.median(area_siembra)]
+	desviacion = [np.std(tamano_finca),np.std(granos_basicos),np.std(area_siembra)]
+	minimo = [min(tamano_finca),min(granos_basicos),min(area_siembra)]
+	maximo = [max(tamano_finca),max(granos_basicos),max(area_siembra)]
+	# agregando a la lista
+	result.append(promedios)
+	result.append(mediana)
+	result.append(desviacion)
+	result.append(minimo)
+	result.append(maximo)
+
+	#distribucion area de siembra
+	menor_1 = filtro.filter(datosmonitoreo__area_siembra__range = (0,0.99)).count()
+	entre_1_2 = filtro.filter(datosmonitoreo__area_siembra__range = (1,2)).count()
+	entre_2_3 = filtro.filter(datosmonitoreo__area_siembra__range = (2.1,3)).count()
+	entre_3_4 = filtro.filter(datosmonitoreo__area_siembra__range = (3.1,4)).count()
+	entre_4_5 = filtro.filter(datosmonitoreo__area_siembra__range = (4.1,5)).count()
 
 	return render(request, template, locals())
 
